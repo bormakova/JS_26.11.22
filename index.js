27cc7180b963889e0f5e60da178b3b1aee19b330
NastyaBormakova <bormakovaanas>
2022-11-26 13:44:46 +0300

    методы добавления элеметов в массивы

---
+++ index.js
@@ -0,0 +1,19 @@
+
+//вывод в консоль массива
+//let array = [5];
+//let array2= new Array(5);
+//console.log(array);
+//console.log(array2);
+
+                //0,       1
+//let fruits = ['Яблоко', 'Апельсин'];
+//console.log(fruits[1]);
+//fruits[0] = 'Груша';
+//console.log(fruits);
+
+let fruits = ['Яблоко', 'Апельсин'];
+//Добавим в конец массива Груша и Мандарин
+// Добавим в начало массива Киви и Лайм
+fruits.push('Груша', 'Мандарин')
+fruits.unshift('Киви', 'Мандарин')
+console.log(fruits);
